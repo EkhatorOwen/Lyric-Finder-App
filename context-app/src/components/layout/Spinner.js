fe6933eeb4269e83bf4fe77ce2af4ab8099c2a11
Owen Ekhator <owenekhator@rocketmail.com>
2018-12-28 16:32:25 -0600

    created spinner component

---
+++ context-app/src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+    <img
+      src="https://cdn.dribbble.com/users/2081/screenshots/2781462/dancing-lines.gif"
+      alt="Loading.."
+      style={{width:'200px',margin:'40px auto',display:'block'}}
+
+    />
+      
+    </div>
+  )
+}
