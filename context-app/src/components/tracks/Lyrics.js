4aff0bcab33d8e1d6b805c2740d6546a89fd77df
Owen Ekhator <owenekhator@rocketmail.com>
2018-12-30 23:07:45 -0600

    added functionality to get artist name and lyrics

--- context-app/src/components/tracks/Lyrics.js
+++ context-app/src/components/tracks/Lyrics.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
 class Lyrics extends Component {
-  state={
+  state = {
     track: {},
     Lyrics: {}
-  }
+  };
 
- async componentDidMount(){
-   //track.lyrics.get?track_id=15953433
-  const result = await axios.get(
-    `http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params}&apikey=${
-      process.env.REACT_APP_MM_KEY
-    }`
-  );
+  async componentDidMount() {
+    //track.lyrics.get?track_id=15953433
 
-  console.log(result.data)
+    const lyrics = await axios.get(
+      `http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${
+        this.props.match.params.id
+      }&apikey=${process.env.REACT_APP_MM_KEY}`
+    );
+
+    const track = await axios.get(
+      `http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${
+        this.props.match.params.id
+      }&apikey=${process.env.REACT_APP_MM_KEY}`
+    );
+
+    //console.log(track.data.message.body.track.track_name)
+
+    this.setState({
+      lyrics: lyrics.data.message.body.lyrics.lyrics_body,
+      track: track.data.message.body.track.track_name
+    });
+
+    //console.log(result.data.message.body.lyrics.lyrics_body)
   }
 
   render() {
@@ -23,9 +37,8 @@ class Lyrics extends Component {
       <div>
         <h1>Lyrics</h1>
       </div>
-    )
+    );
   }
 }
 
-
-export default Lyrics
+export default Lyrics;
