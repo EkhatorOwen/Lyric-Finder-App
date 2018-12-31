84dc519550c8df16ec1ff8c449a4f8c2f6f708ee
Owen Ekhator <owenekhator@rocketmail.com>
2018-12-30 23:04:41 -0600

    added route for track id

--- context-app/src/App.js
+++ context-app/src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
 
-import {Provider} from "./context";
+import { Provider } from "./context";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
           </React.Fragment>
