364bd2924ab2aca30dd7467b9b45ae000a978f20
Owen Ekhator <owenekhator@rocketmail.com>
2019-01-06 01:19:53 -0600

    dispatched result

--- context-app/src/components/tracks/Search.js
+++ context-app/src/components/tracks/Search.js
@@ -7,36 +7,42 @@ class Search extends Component {
     trackTitle: ""
   };
 
-  onChange=(e) => {
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
-    findTrack = (e) => {
+  findTrack = (dispatch, e) => {
     e.preventDefault();
-    let res = await axios.get(
-      `http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${
-        process.env.REACT_APP_MM_KEY
-      }`
-    )
-    
-    // .then(res=>{
-       console.log(res.data)
-    // })
-
-   
-  }
+    console.log(e)
+    axios
+      .get(
+        `http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${
+          this.state.trackTitle
+        }&page_size=10&page=1&s_track_rating=desc&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      )
+      .then(res => {
+        dispatch({
+          type: 'SEARCH_TRACKS',
+          payload: res.data.message.body.track_list
+        })
+        console.log(res.data);
+      });
+  };
 
   render() {
     return (
       <Consumer>
         {value => {
+          const { dispatch } = value
           return (
             <div className="card card-body mb-4 p-4">
               <h1 className="display-4 text-center">
                 <i className="fas fa-music" /> Search For A Song
               </h1>
               <p className="lead text-center">Get the lyrics for any song</p>
-              <form onSubmit={this.findTrack}>
+              <form onSubmit={this.findTrack.bind(this,dispatch)}>
                 <div className="form-group">
                   <input
                     type="text"
@@ -47,7 +53,12 @@ class Search extends Component {
                     onChange={this.onChange}
                   />
                 </div>
-                <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">Get Track Lyrics </button>
+                <button
+                  className="btn btn-primary btn-lg btn-block mb-5"
+                  type="submit"
+                >
+                  Get Track Lyrics{" "}
+                </button>
               </form>
             </div>
           );
